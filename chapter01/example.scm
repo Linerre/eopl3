cd8d101881b1656b57ca360b892ec51aec82b942
Linerre <leon.errelin@outlook.com>
2024-06-24 17:27:45 +1000

    chapter1: Add example and exercises for 1.2.5

--- chapter01/example.scm
+++ chapter01/example.scm
@@ -76,3 +76,61 @@
 ;; => (7, 6)
 (remove-first 5 '(1 2 3 4))
 ;; => (1 2 3 4)
+
+
+;;; 1.2.4
+;;;
+;; occurs-free? : Sym * Lcexp -> Bool
+;; usage:
+;;   returns #t if the symbol var occurs free in exp,
+;;   otherwise returns #f.
+;; Page: 19
+(define occurs-free?
+  (lambda (var exp)
+    (cond
+     ((symbol? exp) (eqv? var exp))
+     ((eqv? (car exp) 'lambda)
+      (and
+       (not (eqv? var (car (cadr exp))))
+       (occurs-free? var (caddr exp))))
+     (else
+      (or
+       (occurs-free? var (car exp))
+       (occurs-free? var (cadr exp)))))))
+
+(occurs-free? 'x 'x)
+
+(occurs-free? 'x 'y)
+;; => #f
+
+(occurs-free? 'x '(lambda (x) (x y)))
+
+(occurs-free? 'x '(lambda (y) (x y)))
+
+(occurs-free? 'x '((lambda (x) x) (x y)))
+
+(occurs-free? 'x '(lambda (y) (lambda (z) (x (y z)))))
+
+
+
+;;; 1.2.5
+;; subst : Sym * Sym * S-list -> S-list
+;; Page: 21
+(define subst
+  (lambda (new old slist)
+    (if (null? slist)
+        '()
+        (cons
+         (subst-in-s-exp new old (car slist))
+         (subst new old (cdr slist))))))
+
+;; subst-in-s-exp : Sym * Sym * S-exp -> S-exp
+;; Page: 21
+(define subst-in-s-exp
+  (lambda (new old sexp)
+    (if (symbol? sexp)
+        (if (eqv? sexp old) new sexp)
+        (subst new old sexp))))
+
+(subst 'a 'b '((b c) (b () d)))
+;; => ((a c) (a () d))
--- chapter01/exercises.scm
+++ chapter01/exercises.scm
@@ -37,3 +37,61 @@
 ;; => ()
 (remove 1 '(2 3 4 5 6))
 ;; => (2 3 4 5 6)
+
+
+;;; 1.11
+;;; the recursion guranteed to halt because (cdr slist) shorter than slist
+
+;;; 1.12
+(define subst
+  (lambda (new old slist)
+    (if (null? slist)
+        '()
+        (cons
+         ;; inline subst-in-s-exp
+         (if (symbol? (car slist))
+             (if (eqv? old (car slist)) new (car slist))
+             (subst new old (car slist)))
+         (subst new old (cdr slist)))
+        )))
+
+(subst 'a 'b '((b c) (b () d)))
+;; => ((a c) (a () d))
+
+;;; 1.13
+;;; I don't know how to make (lambda (slst)) a separate fn
+(define subst-v1
+  (lambda (new old slst)
+    (map
+     (lambda (slst)
+       (if (null? slst)
+           ;; if empty lst then no replacement
+           '()
+           ;; if slst (S-exp) is Symbol, replace as necessary
+           (cons
+            (if (symbol? (car slst))
+                (if (eqv? old (car slst)) new (car slst))
+                (subst new old (car slst)))
+            (subst new old (cdr slst)))))
+     slst)))
+
+(subst-v1 'a 'b '((b c) (b () d)))
+;; => ((a c) (a () d))
+
+;;; FIXME: the following code does not work
+;;; because when mapping substx, it only accepts lists
+(define subst-v2
+  (lambda (new old slst)
+    (map substx new old slst)))
+
+(define (substx new old slst)
+  (cons
+   (if (null? slst)
+      '()
+      (if (symbol? (car slst))
+          (if (eqv? old (car slst)) new (car slst))
+          (substx new old (car slst))))
+   (substx new old (cdr slst))))
+
+(subst-v2 'a 'b '())
+(subst-v2 'a 'b '((b c) (b () d)))
